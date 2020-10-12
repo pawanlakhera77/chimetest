37e8de8c4ca31eb5fe3b24374ce84a1d1910571a
Pawan lakhera <pawanlakhera77@gmail.com>
2020-10-13 01:47:35 +0530

    Updates

---
+++ precache-manifest.939a203f372b5cac4be0a9191b1c5c6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cae5f66e402bbdef8c51c54a327ac300",
+    "url": "/chimetest/index.html"
+  },
+  {
+    "revision": "04d1f5664719d3a0d68e",
+    "url": "/chimetest/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "62b6a4ce82198c4af805",
+    "url": "/chimetest/static/js/2.a1af072d.chunk.js"
+  },
+  {
+    "revision": "643efbae9d6e0fbba269bfc7ee232ed1",
+    "url": "/chimetest/static/js/2.a1af072d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04d1f5664719d3a0d68e",
+    "url": "/chimetest/static/js/main.208d0fb3.chunk.js"
+  },
+  {
+    "revision": "4dca44c6716c4b0e9f24",
+    "url": "/chimetest/static/js/runtime-main.f0951893.js"
+  }
+]);
