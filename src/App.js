612c5c0c1c5f18b07af33ea16f522feef3329dce
Pawan lakhera <pawanlakhera77@gmail.com>
2020-10-18 04:35:32 +0530

    test

--- src/App.js
+++ src/App.js
@@ -17,12 +17,10 @@ function App() {
     <ThemeProvider theme={lightTheme}>
     <MeetingProvider>
       <MyApp />
-      {/* <VideoTileGrid /> */}
-      <div style= {{width:"500",height:"300px"}}>
-          <LocalVideo />
-      </div>
+      <VideoTileGrid />
+      <LocalVideo />
       <MyChild/>
-      {/* <button onClick={toggleVideo}>Toggle video</button> */}
+      {/* //<button onClick={toggleVideo}>Toggle video</button> */}
     </MeetingProvider>
   </ThemeProvider>
   );
@@ -39,3 +37,47 @@ const MyChild = () => {
 };
 
 export default App;
+
+
+
+// import React from 'react';
+// import './App.css';
+// import { ThemeProvider } from 'styled-components';
+// import MyApp from './MyApp.js';
+// import {
+//   MeetingProvider,
+//   lightTheme,
+//   //RemoteVideo,
+//   LocalVideo,
+//   useLocalVideo,
+//   VideoTileGrid
+// } from 'amazon-chime-sdk-component-library-react';
+
+// function App() {
+//   // const { toggleVideo } = useLocalVideo();
+//   return (
+//     <ThemeProvider theme={lightTheme}>
+//     <MeetingProvider>
+//       <MyApp />
+//       {/* <VideoTileGrid /> */}
+//       <div style= {{width:"500",height:"300px"}}>
+//           <LocalVideo />
+//       </div>
+//       <MyChild/>
+//       {/* <button onClick={toggleVideo}>Toggle video</button> */}
+//     </MeetingProvider>
+//   </ThemeProvider>
+//   );
+// }
+
+// const MyChild = () => {
+//   const { isVideoEnabled, toggleVideo } = useLocalVideo();
+
+//   return (
+//     <button onClick={toggleVideo}>
+//       {isVideoEnabled ? 'Stop your video' : 'Start your video'}
+//     </button>
+//   );
+// };
+
+// export default App;
